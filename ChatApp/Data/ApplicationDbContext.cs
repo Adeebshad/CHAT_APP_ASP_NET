55f82eab25b460d033c2bae31378ba0a294dc93a
Adeeb Bin Islam <adeebbinislam@gmail.com>
2021-03-30 21:18:48 -0700

    all basic work done v1.0.1

---
+++ ChatApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ChatApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Message>()
+                .HasOne<AppUser>(a => a.Sender)
+                .WithMany(d => d.Messages)
+                .HasForeignKey(d => d.UserID);
+        }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
